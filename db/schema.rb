ad8762177c315b3a43cf676b341a016b72ca48d4
TimingJL <eefozeo@gmail.com>
2017-12-14 02:25:49 +0800

    add admin to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171213180134) do
+ActiveRecord::Schema.define(version: 20171213182330) do
 
   create_table "links", force: :cascade do |t|
     t.string "titile"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20171213180134) do
     t.string "name"
     t.string "date_of_birth"
     t.boolean "is_female", default: false
+    t.boolean "admin"
+    t.boolean "certification"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
