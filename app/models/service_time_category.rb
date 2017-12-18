8c94470aca0e4cc9df752b7727d1e0030ef037f8
TimingJL <eefozeo@gmail.com>
2017-12-19 04:36:07 +0800

    add dependent: :destroy

--- app/models/service_time_category.rb
+++ app/models/service_time_category.rb
@@ -1,4 +1,4 @@
 class ServiceTimeCategory < ApplicationRecord
   has_many :links_service_times
-  has_many :links, through: :links_service_times
+  has_many :links, through: :links_service_times, dependent: :destroy
 end
--- app/models/service_type_category.rb
+++ app/models/service_type_category.rb
@@ -1,4 +1,4 @@
 class ServiceTypeCategory < ApplicationRecord
   has_many :links_service_types
-  has_many :links, through: :links_service_types
+  has_many :links, through: :links_service_types, dependent: :destroy
 end
