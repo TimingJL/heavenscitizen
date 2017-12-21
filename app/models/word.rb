f423deea6bcfb46980f45a9d29b4fa17c17a59cc
TimingJL <eefozeo@gmail.com>
2017-12-22 06:40:59 +0800

    add require true to word form

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,11 +2,11 @@ class Word < ApplicationRecord
   belongs_to :link
 
   has_many :word_service_times
-  has_many :service_time_categories, through: :word_service_times
+  has_many :service_time_categories, through: :word_service_times, dependent: :destroy
 
   has_many :word_service_types
-  has_many :service_type_categories, through: :word_service_types
+  has_many :service_type_categories, through: :word_service_types, dependent: :destroy
 
   has_many :word_word_contents
-  has_many :word_content_categories, through: :word_word_contents
+  has_many :word_content_categories, through: :word_word_contents, dependent: :destroy
 end
